73bb0b751613509eaf9b6d3ca7c621649586fd5f
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-06-27 14:42:03 +0300

    swipe

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 import Modal from "./modal.js";
 import BrgMenu from "./brgmenu.js";
+import swiper from "./swiper.js";
 
 const modalMenu = new Modal();
 const burgerMenu = new BrgMenu();
+
+
+
 modalMenu.openModal();
 burgerMenu.burgerMenu();
+swiper.init();
+
 // modalMenu.openBurgerModal();
 
 
---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+
+const swiper = new Swiper(`.swiper`, {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+        slidesPerView: 2,
+        centeredSlides: true,
+        spaceBetween: 30,
+});
+
+export default swiper;
+
+        
+
+
